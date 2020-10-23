3e462f7252fc9e21f7dde453000a72fe1810d88c
danielslomba <daniel.jrslomba@gmail.com>
2020-10-23 20:05:51 +0200

    first refactor on updateQuality method "Lift up conditional"

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,55 @@ class GildedRose {
 
     public void updateQuality() {
         for (final Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            handleItem(item);
+        }
+    }
+
+    private void handleItem(final Item item) {
+        switch (item.name) {
+            case "Aged Brie":
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 }
-            }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if ((item).quality < 50) {
+                    (item).quality = (item).quality + 1;
+                    if ((item).sellIn < 11) {
+                        if ((item).quality < 50) {
+                            (item).quality = (item).quality + 1;
+                        }
+                    }
+                    if ((item).sellIn < 6) {
+                        if ((item).quality < 50) {
+                            (item).quality = (item).quality + 1;
+                        }
+                    }
+                }
+                (item).sellIn = (item).sellIn - 1;
+                if ((item).sellIn < 0) {
+                    (item).quality = (item).quality - (item).quality;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+                break;
         }
     }
 }
